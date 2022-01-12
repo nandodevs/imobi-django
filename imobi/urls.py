db64e0c2c636c5ae86e0742ccf08ff6bf05dcca1
Nando Jr <bugdroidgamesbr@gmail.com>
2022-01-12 14:56:24 -0300

    alteracao

--- imobi/urls.py
+++ imobi/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('autenticacao.urls'))
+    path('auth/', include('autenticacao.urls')),
+    path('sair/', views.sair, name="sair")
 ]
