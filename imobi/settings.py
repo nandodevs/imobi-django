7b1d0784ccf01fab80281e46611235a65b995c01
Nando Jr <bugdroidgamesbr@gmail.com>
2022-01-15 18:27:34 -0300

    Corretion

--- imobi/settings.py
+++ imobi/settings.py
@@ -69,16 +69,16 @@ WSGI_APPLICATION = 'imobi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
-DATABASES = {}
+# DATABASES = {}
 
-DATABASES['default'] = dj_database_url.config()
+# DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
