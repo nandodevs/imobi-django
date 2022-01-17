6357435b7d2635af480d81aaa2567c3c9a5600cc
Nando Jr <bugdroidgamesbr@gmail.com>
2022-01-17 16:39:08 -0300

    Status

--- imobi/settings.py
+++ imobi/settings.py
@@ -129,10 +129,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/') #arquivos de media feitos por upload
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #arquivos de media feitos por upload
 MEDIA_URL = '/media/'
 
 # Default primary key field type
